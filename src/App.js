e5a7a92264b483337bac64f325a3fe337634909d
srini047 <srinikethcr7@gmail.com>
2023-02-19 03:34:11 +0530

    add: working without css

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Ideas from "./pages/Ideas";
 import Idea from "./pages/Idea";
+import Description from "./pages/Description";
+import Title from "./pages/Title";
 
 function App() {
   return (
     <BrowserRouter>
         <Routes>
           {/* <Route path="/" element={<Navigate to="home" />} /> */}
-          <Route path="idea/" element={<Idea />} />
+          <Route path="idea" element={<Idea />} />
           <Route path="ideas" element={<Ideas />} />
+          <Route path="description" element={<Description />} />
+          <Route path="title" element={<Title />} />
         </Routes>
     </BrowserRouter>
   );
