802a9c4b82acec559214d7a5c9546ce9ca62c2ce
srini047 <srinikethcr7@gmail.com>
2023-02-19 10:20:38 +0530

    just: basic test added

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,71 +1,27 @@
-import React from 'react';
-import './Test.css';
+// import React, { useState } from 'react';
+// import { Input, Button } from 'antd';
+// import './Test.css';
 
-function App() {
-  return (
-    <div className="container">
-      <h1>My Lists</h1>
-      <form>
-        {/* <label>
-          Name:
-          <input type="text" name="name" />
-        </label> */}
-        <button type="submit">Submit</button>
-      </form>
-      <div className="lists-container">
-        <ul className="list">
-          <li>Item 1</li>
-          <li>Item 2</li>
-          <li>Item 3</li>
-        </ul>
-        <ul className="list">
-          <li>Item 4</li>
-          <li>Item 5</li>
-          <li>Item 6</li>
-        </ul>
-        <ul className="list">
-          <li>Item 7</li>
-          <li>Item 8</li>
-          <li>Item 9</li>
-        </ul>
-        <ul className="list">
-          <li>Item 10</li>
-          <li>Item 11</li>
-          <li>Item 12</li>
-        </ul>
-        <ul className="list">
-          <li>Item 13</li>
-          <li>Item 14</li>
-          <li>Item 15</li>
-        </ul>
-        <ul className="list">
-          <li>Item 16</li>
-          <li>Item 17</li>
-          <li>Item 18</li>
-        </ul>
-        <ul className="list">
-          <li>Item 19</li>
-          <li>Item 20</li>
-          <li>Item 21</li>
-        </ul>
-        <ul className="list">
-          <li>Item 22</li>
-          <li>Item 23</li>
-          <li>Item 24</li>
-        </ul>
-        <ul className="list">
-          <li>Item 25</li>
-          <li>Item 26</li>
-          <li>Item 27</li>
-        </ul>
-        <ul className="list">
-          <li>Item 28</li>
-          <li>Item 29</li>
-          <li>Item 30</li>
-        </ul>
-      </div>
-    </div>
-  );
-}
+// function TextInputPage() {
+//   const [text, setText] = useState('');
 
-export default App;
+//   const handleInputChange = (event) => {
+//     setText(event.target.value);
+//   };
+
+//   const handleButtonClick = () => {
+//     console.log(text);
+//   };
+
+//   return (
+//     <div className="text-input-page">
+//       <Input.TextArea rows={10} value={text} onChange={handleInputChange} />
+//       <Button type="primary" onClick={handleButtonClick}>
+//         Submit
+//       </Button>
+//     </div>
+//   );
+// }
+
+// export default TextInputPage;
+
